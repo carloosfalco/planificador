9c410cb317c3eb1036b7ba42f7be1928e669b113
carloosfalco <carloosfalco@gmail.com>
2025-06-12 15:31:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from rutas import planificador_rutas
-from asignacion_cargas import asignacion_cargas
 from orden_carga_generator import generar_orden_carga
 
 # Configuración de la página
@@ -10,14 +9,11 @@ st.set_page_config(page_title="Virosque TMS", page_icon="🚛", layout="wide")
 st.sidebar.title("📋 Menú")
 seccion = st.sidebar.radio("Selecciona una sección:", [
     "Planificador de Rutas",
-    "Asignación de Cargas",
     "Orden de Carga"
 ])
 
 # Mostrar la sección seleccionada
 if seccion == "Planificador de Rutas":
     planificador_rutas()
-elif seccion == "Asignación de Cargas":
-    asignacion_cargas()
 elif seccion == "Orden de Carga":
     generar_orden_carga()

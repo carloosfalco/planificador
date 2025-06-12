f667aee338ddfef9bac295ff6d3d6e95ddfb4155
carloosfalco <carloosfalco@gmail.com>
2025-06-12 15:36:43 +0200

    Update rutas.py

--- rutas.py
+++ rutas.py
@@ -35,10 +35,10 @@ def planificador_rutas():
 
     # Logo y título
     logo = Image.open("logo-virosque2-01.png")
-    col1, col2 = st.columns([1, 2])
-    with col1:
-        st.image(logo, width=200)
+    col1, col2 = st.columns([1, 3])
     with col2:
+        st.image(logo, width=200)
+    with col1:
         st.markdown("<h1 style='color:#8D1B2D;'>TMS</h1>", unsafe_allow_html=True)
         st.markdown("### Planificador de rutas para camiones", unsafe_allow_html=True)
 

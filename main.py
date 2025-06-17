5303002fa0a1aecc127180c11eae66fb7a7ee6e8
carloosfalco <carloosfalco@gmail.com>
2025-06-17 12:53:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import streamlit as st
 from rutas import planificador_rutas
-from orden_carga_generator import generar_instrucciones_ruta  # ✅ nombre correcto del archivo
-from calendario import calendario_eventos  # ✅ nuevo módulo del calendario
+from orden_carga import orden_carga
+from calendario import calendario_eventos
 from matriculas import matriculas
 
 def main():
     st.set_page_config(page_title="Virosque TMS", page_icon="🚛", layout="wide")
 
     st.sidebar.title("📂 Menú")
-    seleccion = st.sidebar.radio("Selecciona una opción", ["Planificador de rutas", "Orden de carga", "Calendario de eventos", "Matrículas"])
+    seleccion = st.sidebar.radio("Selecciona una opción", [
+        "Planificador de rutas",
+        "Orden de carga",
+        "Calendario de eventos",
+        "Matrículas"
+    ])
 
     if seleccion == "Planificador de rutas":
         planificador_rutas()
     elif seleccion == "Orden de carga":
-        generar_instrucciones_ruta()
+        orden_carga()
     elif seleccion == "Calendario de eventos":
         calendario_eventos()
     elif seleccion == "Matrículas":
-    matriculas()
+        matriculas()
 
 if __name__ == "__main__":
     main()
